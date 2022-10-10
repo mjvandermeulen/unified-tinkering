6c141910d454f7044a90daa5519b42c5d7743572
Maarten van der Meulen <mjvandermeulen@gmail.com>
2022-10-10 17:29:14 -0400

    nodemon working with TS and ESM. awesome!

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,1 +1,8 @@
-console.log("hello main world")
+// ESM module:
+// https://2ality.com/2021/06/typescript-esm-nodejs.html
+
+//nodemon, typescript with ESM:
+// https://www.rockyourcode.com/nodejs-setup-with-typescript-nodemon-and-esm/
+
+console.log();
+console.log("hello main world");
