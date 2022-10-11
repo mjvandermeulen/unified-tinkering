ca262f27c98ef01823e7e0749fde5bfc9244a7c1
Maarten van der Meulen <mjvandermeulen@gmail.com>
2022-10-11 13:15:53 -0400

    nodemon now truly compiling.

---
+++ ts/src/markdown/ontario_and_quebec.ts
@@ -0,0 +1,22 @@
+const markdown = `
+#### mvdm-Header4.
+
+mvdm first line.
+
+**mvdm-BOLD.**
+
+-
+    -   :mvdm-radio:
+    -   :mvdm-header:
+    -   ( ) a | mvdm-a-label.
+    -   [ ] b | mvdm-b-label.
+    -   [ ] c | mvdm-c-label.
+        1. mvdm-ordered-list-item 1.
+        1. mvdm-ordered-list-item 2.
+    -   [ ] b | mvdm-d-label.
+    -   [ ] c | mvdm-e-label.
+
+_italics and end._
+`;
+
+export default markdown;
--- ts/src/unified-tinker.ts
+++ ts/src/unified-tinker.ts
@@ -4,25 +4,27 @@ import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import rehypeReact from "rehype-react"
+import remarkStringify from "remark-stringify";
 import rehypeStringify from "rehype-stringify";
+import markdown from "./markdown/ontario_and_quebec.js"
 
 async function main() {
     const file = await unified()
         .use(remarkParse)
         .use(remarkGfm)
-        .use(remarkRehype)
-        .use(rehypeReact, {createElement, Fragment})
-        .use(rehypeStringify)
-        .process(markdown);
+
+
+        
+        // .use(remarkStringify)
+        // .use(remarkRehype)
+        // .use(rehypeReact, {createElement, Fragment})
+        // .use(rehypeStringify)
+        .parse(markdown);
 
     console.log();
-    console.log(String(file));
+    console.log(JSON.stringify(file, null, 4));
     console.log();
 }
-
-const markdown = `
----\nlayout: home\n---\n\n# Hi ~~Mars~~Venus!
-`
 
 main();
 
